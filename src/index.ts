8e76a74cd1d9635bb4e1ba937b590bac28825754
leeyc924 <leeyc924@naver.com>
2023-08-20 22:03:22 +0900

    Modify typescript migration

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,46 @@
-// Returns a random integer from 0 to range - 1.
-function randomInt(range) {
-  return Math.floor(Math.random() * range);
-}
+import { createProgram, createShader, randomInt, resizeCanvasToDisplaySize, setRectangle } from './utils';
 
-// Fill the buffer with the values that define a rectangle.
-function setRectangle(gl, x, y, width, height) {
-  const x1 = x;
-  const x2 = x + width;
-  const y1 = y;
-  const y2 = y + height;
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-     x1, y1,
-     x2, y1,
-     x1, y2,
-     x1, y2,
-     x2, y1,
-     x2, y2,
-  ]), gl.STATIC_DRAW);
-}
+const vertexShaderSource = `
+  attribute vec2 a_position;
+
+  uniform vec2 u_resolution;
+
+  void main() {
+    vec2 zeroToOne = a_position / u_resolution;
+    vec2 zeroToTwo = zeroToOne * 2.0;
+    vec2 clipSpace = zeroToTwo - 1.0;
+    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+  }
+`;
+const fragmentShaderSource = `
+  precision mediump float;
+
+  uniform vec4 u_color;
+
+  void main() {
+    gl_FragColor = u_color;
+  }
+`;
 
 function main() {
-  const canvas = document.getElementById("canvas");
-  const gl = canvas.getContext("webgl");
+  const canvas = <HTMLCanvasElement>document.getElementById('canvas');
+  const gl = canvas.getContext('webgl');
 
   if (!gl) {
     return;
   }
-  // Link the two shaders into a program
-  const program = webglUtils.createProgramFromScripts(gl, [
-    "vertex-shader-2d",
-    "fragment-shader-2d",
-  ]);
+
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+  const program = createProgram(gl, vertexShader, fragmentShader);
 
   // look up where the vertex data needs to go.
-  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
 
   // look up uniform locations
-  const resolutionUniformLocation = gl.getUniformLocation(
-    program,
-    "u_resolution"
-  );
-  const colorUniformLocation = gl.getUniformLocation(program, "u_color");
+  const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
+  const colorUniformLocation = gl.getUniformLocation(program, 'u_color');
 
   // Create a buffer to put three 2d clip space points in
   const positionBuffer = gl.createBuffer();
@@ -48,7 +48,7 @@ function main() {
   // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+  resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
 
   // Tell WebGL how to convert from clip space to pixels
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -72,14 +72,7 @@ function main() {
   const normalize = false; // don't normalize the data
   const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
   const pOffset = 0; // start at the beginning of the buffer
-  gl.vertexAttribPointer(
-    positionAttributeLocation,
-    size,
-    type,
-    normalize,
-    stride,
-    pOffset
-  );
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, pOffset);
 
   // set the resolution
   gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
@@ -90,22 +83,10 @@ function main() {
     // This will write to positionBuffer because
     // its the last thing we bound on the ARRAY_BUFFER
     // bind point
-    setRectangle(
-      gl,
-      randomInt(300),
-      randomInt(300),
-      randomInt(300),
-      randomInt(300)
-    );
+    setRectangle(gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
 
     // Set a random color.
-    gl.uniform4f(
-      colorUniformLocation,
-      Math.random(),
-      Math.random(),
-      Math.random(),
-      1
-    );
+    gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
 
     // Draw the rectangle.
     var primitiveType = gl.TRIANGLES;
---
+++ src/utils.ts
@@ -0,0 +1,49 @@
+export function createShader(gl: WebGLRenderingContext, type: number, source: string) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  gl.deleteShader(shader);
+}
+
+export function createProgram(gl: WebGLRenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader) {
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  gl.deleteProgram(program);
+}
+
+export function randomInt(range: number) {
+  return Math.floor(Math.random() * range);
+}
+
+export function setRectangle(gl: WebGLRenderingContext, x: number, y: number, width: number, height: number) {
+  const x1 = x;
+  const x2 = x + width;
+  const y1 = y;
+  const y2 = y + height;
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]), gl.STATIC_DRAW);
+}
+
+export function resizeCanvasToDisplaySize(canvas: HTMLCanvasElement, multiplier = 1) {
+  const width = (canvas.clientWidth * multiplier) | 0;
+  const height = (canvas.clientHeight * multiplier) | 0;
+  if (canvas.width !== width || canvas.height !== height) {
+    canvas.width = width;
+    canvas.height = height;
+    return true;
+  }
+  return false;
+}
